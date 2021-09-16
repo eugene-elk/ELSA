dba4a9b9d7cb5df9e614c758ae7b9d908ee5905f
Levon <l.pogosov13@gmail.com>
2021-09-17 00:38:41 +0300

    added comments for improvization python code

--- main.py
+++ main.py
@@ -1,47 +1,53 @@
 import xgboost as xgb
 import numpy as np
 import sounddevice as sd
-from math import atan2, pi, sqrt
+from math import atan2, nan, pi, sqrt
 
-from bbox_ops import *
 from stuff import *
+from melody_generator import MelodyGenerator
 
 duration = 10
-sound_border = 0.1
+sound_border = 0.3
 
+# Узнаем сэмплрейт (частота записи звука) дефолтного микрофона
 samplerate = sd.query_devices(None, 'input')['default_samplerate']
 
 import serial, time
 arduino = serial.Serial('/dev/ttyUSB0', 115200)
 time.sleep(1) #give the connection a second to settle
 
+# Загружаем модельку
 bst = xgb.Booster()
 bst.load_model('0001.model')
 
 a = input()
 
+# Флаг ожидания звука
 waiting = True
 print('waiting')
 
+# Обработчик данных с микрофона
 def callback(indata, frames, time, status):
+
     if status:
         print(status)
+
     if any(indata):
-        
+        # Если есть доступные данные - считаем среднюю мощность звука
         mean = np.sqrt(np.mean(indata**2)) 
         print(mean)
-
+        
         if (mean > sound_border) :
+            # Если звук достаточно громкий - перестаём ждать
             global waiting 
             waiting = False
-
     else:
         print('no input')
 
-def wait_sound(a = 0.3):
-    global waiting, sound_border
+
+def wait_sound():
+    global waiting
     waiting = True
-    sound_border = a
 
     with sd.InputStream(channels=1, callback=callback, blocksize=int(samplerate * 0.05), samplerate=samplerate):
         while waiting:
@@ -63,34 +69,29 @@ wait_sound()
 arduino.write([1])
 arduino.read()
 
+# Записываем duration секунд
 myrecording = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1)
+# Ждём пока запишется
 sd.wait()
+# Распознаём аккорды
 sequence = process_recording(myrecording, samplerate, bst)
 print(sequence)
 
-
-from melody_generator import MelodyGenerator
-
+# Создаём генератор, передаём ему первые 4 аккорда, 8 четвертых в одном аккорде (скорее всего)
 gen = MelodyGenerator()
 melody = gen.generate(sequence[:4], 8)
-
+# Добавляем знак конца мелодии (либо тоника)
 melody.extend([20, 0])
 
+# Передача мелодии на Ардуино
 for i in range(len(melody) // 2):
-    
     arduino.write([melody[i*2], melody[i*2 + 1]])
     arduino.read()
 
 time.sleep(0.5)
 
+# Ждём начала игры гитариста
 wait_sound()
 arduino.write([1])
 
 arduino.read()
-
-def wait_a(a):
-    global waiting
-    waiting = True
-    a.read()
-    waiting = False
-    print('!!!!')
