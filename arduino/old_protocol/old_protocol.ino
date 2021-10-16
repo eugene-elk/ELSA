ab059fb893584849d8962d2b0cea2ae1ffeae2aa
Evgeny Lositsky <elosyash239@gmail.com>
2021-10-16 05:35:34 +0300

    created new_protocol with note class

--- arduino/old_protocol/old_protocol.ino
+++ arduino/old_protocol/old_protocol.ino
@@ -1,97 +1,5 @@
 #include <Adafruit_PWMServoDriver.h>
-
-// Максимальный и минимальный углы поворота сервы
-#define SERVOMIN 80
-#define SERVOMAX 600
-
-// Концевики на движение рук вверх и вниз
-#define UPBORDER 9
-#define DOWNBORDER 5 
-
-// Моторы на поднятие и опускание рук
-#define ARMSMOTORUP 50
-#define ARMSMOTORDOWN 48
-
-// Порт клапана
-#define VALVE 13 //HIGH -- off, LOW -- on
-
-// Порт компрессора
-#define PUMP 17 //HIGH -- off, LOW -- on
-
-// хз что это
-#define LCD 3
-
-// Номер сервы головы
-const short HEAD_SERVO = 0; 
-
-// Положения сервы головы
-const short HEAD_FRONT = 47;
-const short HEAD_LEFT = 30;
-
-class note {
-
-    note() // По умолчанию создаём ноту До (C)
-    {
-        name = "C";
-        fingerings = {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    }
-
-    public:
-        String name;
-        short[11] fingerings;
-
-        note(String _name, short[11] _fingerings) {
-            name = _name;
-            ingerings = _fingerings;
-        }
-}
-
-// Даёт положения пальцев для выбранной ноты
-note fingerings[14] = {
-    note("C",   {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1}), 
-    note("C#",  {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0}), 
-    note("D",   {1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 1}),  
-    note("D#",  {1, 2, 1, 1, 1, 1, 1, 1, 0, 0, 1}),  
-    note("E",   {1, 2, 1, 1, 1, 1, 1, 0, 0, 1, 1}),  
-    note("F",   {1, 2, 1, 1, 1, 1, 0, 0, 0, 1, 1}),  
-    note("F#",  {1, 2, 1, 1, 1, 0, 1, 1, 1, 1, 1}),  
-    note("G",   {1, 2, 1, 1, 1, 0, 0, 0, 0, 1, 1}),  
-    note("G#",  {1, 2, 1, 1, 0, 1, 1, 0, 0, 1, 1}),  
-    note("A",   {1, 2, 1, 0, 1, 1, 1, 0, 1, 1, 1}),  
-    note("A#",  {1, 2, 1, 0, 1, 0, 0, 0, 0, 1, 1}),  
-    note("B",   {1, 2, 0, 1, 0, 1, 0, 1, 0, 1, 1}),  
-    note("C_2", {1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1}),  
-    note("C#_2",{1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1})
-}
-    // дальше надо сильнее дуть, пока мы не можем
-
-    {1, 2, 1, 1, 1, 1, 1, 0, 0, 0, 1},  //from D# 1     18
-    {1, 2, 1, 1, 1, 1, 1, 0, 0, 1, 1},  //from D# 2     19
-    {1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 0},  //from C# 1     20
-    {1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 1}}; //from C# 2     21
-    
-// Положение серв пальцев при открытии и закрытии ноты
-short positions[11][3] = {
-// open, closed
-    {0, 0},     //head
-    {5, 20, 40},   //1
-    {25, 50},   //2
-    {60, 20},   //3
-    {20, 55},   //4
-    {70, 20},   //5
-    {60, 20},   //6
-    {40, 65},   //7
-    {70, 20},   //8
-    {30, 10},   //9
-    {22, 5}};  //10
-
-// Соответствие виртуальных и физических номеров серв
-// Виртуальный - номер в массиве
-// Физический - номер порта на Ардуино
-//                0, 1, 2, 3, 4,  5,  6,  7,  8,  9, 10
-short servos[] = {0, 1, 2, 3, 4, 13, 15, 12, 10, 14, 11};
-
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+#include "/home/tlab/Documents/ELSA/arduino/settings.h"
 
 // Создаем сервоконтроллер
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
@@ -143,16 +51,19 @@ void setup()
     delay(100);
 }
 
-// Взятие ноты через объект ноты
-void pick_note(note note)
+// Взятие ноты
+void pick_note(uint8_t note)
 {
-    for (int i = 0; i < 11; i++) 
+    // Перебираем все 11 подключенных серв
+    for (int i = 0; i < 11; ++i)
     {
+        // Если наткнулись на серву головы - пропускаем
         if (i == HEAD_SERVO) 
             continue;
-        
-        int p = get_pulse(note.fingerings[i]);
+        // Записываем в переменную p нужную позицию для сервы i по ноте note
+        int p = get_pulse(positions[i][fingerings[note][i]]);
 
+        //Выставляем по физическому номеру сервы её в нужное положение
         pwm.setPWM(servos[i], 0, p);
     }
 }
@@ -223,43 +134,39 @@ void loop()
 
             // Переход в D#
             if (current_note == 3) {
-                note to_D_dies_1("to D# 1", {1, 2, 1, 1, 1, 1, 1, 0, 0, 1, 1});
-                note to_D_dies_2("to D# 2", {1, 2, 1, 1, 1, 1, 1, 0, 0, 0, 1});
-                pick_note(to_D_dies_1);
+                pick_note(14);
                 delay(20);
-                pick_note(to_D_dies_2);
+                pick_note(15);
                 delay(20); 
             }
             // Переход в С#
             if (current_note == 1) {
-                note to_C_dies_1("to C# 1", {1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 1});
-                note to_C_dies_2("to C# 2", {1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 0});
-                pick_note(to_C_dies_1);
+                pick_note(16);
                 delay(20);
-                pick_note(to_C_dies_2);
+                pick_note(17);
                 delay(20); 
+                // pwm.setPWM(servos[10], 0, positions[10][0]);
+                // delay(1000);
+                // pwm.setPWM(servos[10], 0, positions[10][0]);
+                // delay(200);
             }
             // Переход из D#
             if (previous_note == 3) {
-                note from_D_dies_1("from D# 1", {1, 2, 1, 1, 1, 1, 1, 0, 0, 0, 1});
-                note from_D_dies_2("from D# 2", {1, 2, 1, 1, 1, 1, 1, 0, 0, 1, 1});
-                pick_note(from_D_dies_1);
+                pick_note(18);
                 delay(20);
-                pick_note(from_D_dies_2);
+                pick_note(19);
                 delay(20); 
             }
             // Переход из С#
             if (previous_note == 1) {
-                note from_C_dies_1("from C# 1", {1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 0});
-                note from_C_dies_2("from C# 2", {1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 1});
-                pick_note(from_C_dies_1);
+                pick_note(20);
                 delay(20);
-                pick_note(from_C_dies_2);
+                pick_note(21);
                 delay(20); 
             }
 
             // Взятие ноты
-            pick_note(fingerings[sequence[i][0]]);
+            pick_note(sequence[i][0]);
             
             // Действия с клапаном и компрессором (?)
             if (sequence[i][1] > 0)
